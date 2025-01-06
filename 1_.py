3b38a537f4ec0b3de6dc4f3eb75781c59b3adabe
Frias <jdfrias@cps.k12.il.us>
2025-01-06 12:31:31 -0600

    working on it

--- 1_.py
+++ 1_.py
@@ -1,34 +1,50 @@
-# Methods, Help & Documentation Practice #1
-# Remove the characters to the left of our main text:
+# # functions are ways to wrap your code 
+# #into reuseable units
+# # function = a block of resuable code
+#                 #place() after the function name to invoke it
+# def happy_birthday(name, age):  #parameter, placeholders to make the function                  #define the function
+#     print(f"Happy birthday to {name}!")
+#     print(f"You are {age} years old!")
+#     print(f"Happy birthday to {name}!")        
+#     print()
 
-# ,
+# happy_birthday("bro", 20) 
+# happy_birthday("steve", 30)
+# happy_birthday("joe", 40 ) #argument when you call the function         #invoke the function 
 
-# :
+# def display_invoice(username, amount, due_date):
+#    print(f"Hello {username}")
+#    print(f"Your bill of ${amount:.2f} is due: {due_date}")
 
-# %
-
-# _
-
-# #
-
-# Use the lstrip() method. Print the result to the screen:
-
-# ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
-
-# Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+# display_invoice("BroCode", 42.50, "01/02")
+# display_invoice("JoeSchmo", 100.99, "02/03")
 
 
-# Methods, Help & Documentation Practice #2
-# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+def add (x,y):
+   z=x+y
+   return z
 
-# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+def subtract (x,y):
+   z=x-y
+   return z
 
-# Search the documentation for the requested method to know how it works.
+def multiply (x,y):
+   z=x-y
+   return z
 
-# Methods, Help & Documentation Practice #3
-# Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
+def divide (x,y):
+   z=x/y
+   return z
 
-# phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-# tv_brands = {"Sony", "Philips", "Samsung", "LG"}
-# Search the documentation for the requested method to know how it works.
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
 
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
